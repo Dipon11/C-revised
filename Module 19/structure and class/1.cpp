18351dae7d9e8d8cbf80e6412fab9261c55b2f55
Dipon11 <dipondas1132@gmail.com>
2024-05-06 23:31:58 +0600

    QUiz tommorow

--- Module 19/structure and class/1.cpp
+++ Module 19/structure and class/1.cpp
@@ -8,3 +8,26 @@ public:
    int cls;
    char section;
 };
+int main()
+{
+
+   Student rahim;
+   rahim.roll = 29;
+   rahim.cls = 9;
+   rahim.section = 'A';
+   char nm[100] = "Rahim";
+   strcpy(rahim.name, nm);
+
+   Student karim;
+   karim.roll = 55;
+   karim.cls = 9;
+   karim.section = 'C';
+   char nm2[100] = "Karim";
+   strcpy(karim.name, nm2);
+
+   cout << rahim.roll << " " << rahim.section << endl;
+
+   cout << karim.roll << " " << karim.name << endl;
+
+   return 0;
+}
