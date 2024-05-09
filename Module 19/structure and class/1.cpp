70e1a65d214c98b2fe0f204be91c0fd179e6d403
Dipon11 <dipondas1132@gmail.com>
2024-05-10 00:57:33 +0600

    Strcture

--- Module 19/structure and class/1.cpp
+++ Module 19/structure and class/1.cpp
@@ -13,25 +13,23 @@ int main()
 {
 
    laptop hp;
-   char nm[100]="Hp";
+   char nm[100] = "Hp";
    strcpy(hp.name, nm);
-   char ny[100]="Windows";
+   char ny[100] = "Windows";
    strcpy(hp.osname, ny);
-   hp.ram=8;
-   hp.ssd=128;
-
+   hp.ram = 8;
+   hp.ssd = 128;
 
    laptop asus;
-   char nt[100]="Asus";
+   char nt[100] = "Asus";
    strcpy(asus.name, nt);
-   char nb[100]="Windows";
+   char nb[100] = "Windows";
    strcpy(asus.osname, nb);
-   asus.ram=8;
-   asus.ssd=256;
+   asus.ram = 8;
+   asus.ssd = 256;
 
    cout << hp.ram << " " << asus.ram << endl;
-    cout<< hp.name<<" "<<asus.name  <<endl;
-   
+   cout << hp.name << " " << asus.name << endl;
 
    return 0;
 }
