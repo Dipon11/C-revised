2cf999ef4216dbfb0d88041413ee571de917ae15
Dipon11 <dipondas1132@gmail.com>
2024-05-14 23:54:26 +0600

    gsdgsd

--- C ++Module 2.5/Sorting.cpp
+++ C ++Module 2.5/Sorting.cpp
@@ -13,22 +13,19 @@ int main()
         cin >> arr[i];
     }
 
-
-
     for (int i = 0; i <= n - 1; i++)
     {
-        for (int j = i+1; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
             if (arr[i] > arr[j])
             {
                 swap(arr[i], arr[j]);
             }
-           
         }
     }
     for (int i = 0; i < n; i++)
     {
-         cout<< arr[i]  <<endl;
+        cout << arr[i] << endl;
     }
 
     return 0;
